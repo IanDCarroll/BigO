d63cf642b450a6f84627306e1b4cb7678be0e9ba
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-13 21:50:17 -0800

    add first test

---
+++ big_o_test.py
@@ -0,0 +1,12 @@
+import nose.tools
+from big_o import O_of_1
+
+test = O_of_1()
+
+test_list = []
+for i in range(0, 10,000):
+    test_list.append(i)
+
+
+def test_O_of_1():
+    assert O_of_1.run(test_list) == True

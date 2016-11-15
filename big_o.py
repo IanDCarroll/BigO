0c4b318f8a20727d69b3ab560d959522d2ca43df
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-14 18:20:52 -0800

    clarify purpose of BigO_of_1

--- big_o.py
+++ big_o.py
@@ -1,6 +1,6 @@
 class BigO_of_1(object):
-    def check_index_0(self, value_list):
-        if value_list[0]:
+    def check_index_0_is_int(self, value_list):
+        if value_list[0] == int(value_list[0]):
            return True
 
 class BigO_of_N(object):
--- big_o_test.py
+++ big_o_test.py
@@ -5,12 +5,12 @@ test_of_1 = BigO_of_1()
 test_of_N = BigO_of_N()
 
 test_list = []
-for i in range(1, 10000):
+for i in range(0, 10000):
     test_list.append(i)
 
 
 def test_O_of_1_tells_truth():
-    assert test_of_1.check_index_0(test_list) == True
+    assert test_of_1.check_index_0_is_int(test_list) == True
 
 def test_O_of_N_Doubles_lists():
     returned_list = test_of_N.double_values(test_list)

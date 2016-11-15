3085db6f5d16da7c0cc3455d82acda9d9706c4ea
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-14 18:06:08 -0800

    refactor test name for added clarity

--- big_o_test.py
+++ big_o_test.py
@@ -12,7 +12,7 @@ for i in range(1, 10000):
 def test_O_of_1_tells_truth():
     assert test_of_1.check_index_0(test_list) == True
 
-def test_O_of_N():
+def test_O_of_N_Doubles_lists():
     returned_list = test_of_N.double_values(test_list)
     assert returned_list[880] == test_list[880] * 2
  

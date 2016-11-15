1b2b6afa666b2659ad2d28b07dcec9c7b923511f
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-14 21:50:27 -0800

    reorder tests from shortest to longest

--- big_o_test.py
+++ big_o_test.py
@@ -31,6 +31,19 @@ def test_O_of_2_N_Gives_the_right_factorial():
     result = test_of_2_N.get_factorial(100)
     assert result == one_hundred_factorial
  
+def test_O_of_N_log_N():
+    returned_list = test_of_N_log_N.sort_list(random_test_list)
+    returned_boolean = True
+    if returned_list[0] > returned_list[99]:
+        returned_boolean = False
+    if returned_list[55] > returned_list[56]:
+        returned_boolean = False
+    if returned_list[33] > returned_list[42]:
+        returned_boolean = False
+    if returned_list[76] > returned_list[88]:
+        returned_boolean = False
+    assert returned_boolean == True
+
 def test_O_of_N_Squared_makes_a_spam_field():
     returned_list = test_of_N_Squared.create_spam_field(test_list)
     assert len(returned_list) == 100
@@ -51,16 +64,3 @@ def test_O_of_N_Four_Makes_a_spam_hyperspace():
     assert len(returned_list[88][88]) == 100
     assert len(returned_list[88][88][88]) == 100
     assert returned_list[88][88][88][88] == 'spam'
-
-def test_O_of_N_log_N():
-    returned_list = test_of_N_log_N.sort_list(random_test_list)
-    returned_boolean = True
-    if returned_list[0] > returned_list[99]:
-        returned_boolean = False
-    if returned_list[55] > returned_list[56]:
-        returned_boolean = False
-    if returned_list[33] > returned_list[42]:
-        returned_boolean = False
-    if returned_list[76] > returned_list[88]:
-        returned_boolean = False
-    assert returned_boolean == True
